4f896079510bcbc90b4426fcacd29110c139ec36
ninehcobra <ttbexinhtt2903@gmail.com>
2024-04-18 17:58:12 +0700

    fix

--- src/app/ver3/page.js
+++ src/app/ver3/page.js
@@ -1,8 +1,25 @@
 "use client";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+
+// Kiểm tra xem có đang chạy trên trình duyệt hay không
+const isBrowser = typeof window !== "undefined";
 
 export default function Ver3() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Đảm bảo rằng thư viện Leaflet đã được load khi chạy trên trình duyệt
+    if (isBrowser) {
+      import("leaflet").then(() => {
+        setIsLoaded(true);
+      });
+    }
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <MapContainer center={[10.774, 106.7006]} zoom={8} scrollWheelZoom={true}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
